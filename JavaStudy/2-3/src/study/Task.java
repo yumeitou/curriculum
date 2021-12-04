ec94ddf0a2ad7df97df49142deac40517922cb18
yumeitou <93590605+yumeitou@users.noreply.github.com>
2021-12-04 23:24:40 +0900

    java2-3 課題提出
    java2-3 課題提出

--- JavaStudy/2-3/src/study/Task.java
+++ JavaStudy/2-3/src/study/Task.java
@@ -11,9 +11,9 @@ public class Task extends Calculator {
 
 	        // ② Calculator.javaのすべてのオーバーロードメソッド「plus」に適当な引数を与え、下記画像のよう出力されるようコーディングしなさい。
 	        // 尚、「どのクラスから呼び出しているか」を明確にするため、plus()には呼び出し元のキーワードを付与すること。
-	    	System.out.println("pulsメソッドの引数が一つの場合：" + plus(1));
-	    	System.out.println("pulsメソッドの引数が二つの場合：" + plus(1, 2));
-	    	System.out.println("pulsメソッドの引数が三つの場合：" + plus(1, 2, 3));
+	    	System.out.println("pulsメソッドの引数が一つの場合：" + plus(10));
+	    	System.out.println("pulsメソッドの引数が二つの場合：" + plus(10, 20));
+	    	System.out.println("pulsメソッドの引数が三つの場合：" + plus(10, 20, 30));
 
 	    }
 	}
